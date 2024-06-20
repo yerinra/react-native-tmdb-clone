5825a5e8a77ff31d49d7ac7a37d97b994afe47cf
yerinra <yerinaleph@gmail.com>
2024-06-21 00:21:36 +0900

    feat: 랜딩 페이지 구현

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,38 @@
-import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect, router } from "expo-router";
 
 export default function App() {
 	return (
-		<View className="flex-1 items-center justify-center bg-white">
-			<Text className="text-3xl font-interMedium">TMDB clone!</Text>
-			<StatusBar style="auto" />
-			<Link href="/profile">Go to Profile</Link>
-		</View>
+		<SafeAreaView className="bg-primary h-full">
+			<ScrollView contentContainerStyle={{ height: "100%" }}>
+				<View className="w-full justify-center items-center h-full">
+					<Text className="text-5xl text-secondary font-interBlack">THE</Text>
+					<Text className="text-5xl text-secondary font-interBlack">MOVIE</Text>
+					<Text className="text-5xl text-secondary font-interBlack">DB</Text>
+					<Text className="text-white text-sm font-interRegular mt-5 text-center">
+						Millions of movies, TV shows
+					</Text>
+					<Text className="text-white text-sm font-interRegular text-center">
+						and people to discover.
+					</Text>
+					<Text className="text-white text-lg font-interSemiBold mt-3 text-center">
+						Explore now.
+					</Text>
+					<CustomButton
+						title="Sign In"
+						handlePress={() => {
+							router.push("/sign-in");
+						}}
+						containerStyles="w-11/12 mt-14"
+						isLoading={false}
+					/>
+				</View>
+				<TouchableOpacity></TouchableOpacity>
+			</ScrollView>
+			<StatusBar backgroundColor="#0d253f" style="light" />
+		</SafeAreaView>
 	);
 }
---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,31 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+type CustomButtonProps = {
+	title: string;
+	handlePress: () => void;
+	isLoading: boolean;
+	containerStyles?: string;
+	textStyles?: string;
+};
+
+const CustomButton = ({
+	title,
+	handlePress,
+	isLoading,
+	containerStyles,
+	textStyles,
+}: CustomButtonProps) => {
+	return (
+		<TouchableOpacity
+			onPress={handlePress}
+			activeOpacity={0.7}
+			className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? "opacity-50" : ""}`}
+			disabled={isLoading}
+		>
+			<Text className={`text-primary font-interSemiBold text-lg ${textStyles}`}>{title}</Text>
+		</TouchableOpacity>
+	);
+};
+
+export default CustomButton;
