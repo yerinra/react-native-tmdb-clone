7d6f46ce7b6bb03a63c90598922f51a261d896be
yerinra <yerinaleph@gmail.com>
2024-06-22 01:32:31 +0900

    feat: Empty 컴포넌트 구현

---
+++ src/components/Empty.tsx
@@ -0,0 +1,29 @@
+import { View, Text } from "react-native";
+import React from "react";
+import CustomButton from "./CustomButton";
+import { router } from "expo-router";
+
+type EmptyProps = {
+	title: string;
+	desc: string;
+	showButton: boolean;
+};
+
+const Empty = ({ title, desc, showButton }: EmptyProps) => {
+	return (
+		<View className="justify-center items-center px-4">
+			<Text className="text-xl text-center font-interSemiBold text-white mt-2">{title}</Text>
+			<Text className="text-sm text-text font-interMedium">{desc}</Text>
+			{showButton && (
+				<CustomButton
+					isLoading={false}
+					title="Search Again"
+					handlePress={() => router.push("/home")}
+					containerStyles="w-full my-5 mt-10"
+				/>
+			)}
+		</View>
+	);
+};
+
+export default Empty;
