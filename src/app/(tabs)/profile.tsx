279ded5c17b4311a096d3a3d04bee9f70d7a4ab1
yerinra <yerinaleph@gmail.com>
2024-06-21 17:07:48 +0900

    feat: 로그아웃 기능 구현

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,31 @@
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import React from "react";
+import CustomButton from "@/components/CustomButton";
+import { signOut } from "@/lib/appwrite";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { clearUser } from "@/redux/slice/userSlice";
+import { Link, router } from "expo-router";
 
 const Profile = () => {
+	const dispatch = useAppDispatch();
+	const { user } = useAppSelector((state) => state.user);
+	const handlePress = async () => {
+		try {
+			await signOut();
+			dispatch(clearUser());
+			router.replace("/");
+		} catch (error: any) {
+			Alert.alert("Error", error.message);
+		}
+	};
+
 	return (
 		<View>
 			<Text>Profile</Text>
+			<Text>{user?.$createdAt}</Text>
+
+			<Link href="/sign-in">로긴</Link>
+			<CustomButton title="Sign Out" handlePress={handlePress} isLoading={false}></CustomButton>
 		</View>
 	);
 };
