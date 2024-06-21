aff9ad4da066eac6d04c200dcd47c9f0d1605190
yerinra <yerinaleph@gmail.com>
2024-06-21 23:51:53 +0900

    refactor: url 상수로 분리

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://api.themoviedb.org/3/movie/";
+export const SEARCH_URL = "https://api.themoviedb.org/3/search/movie";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/";
